9ff8f1b50388e5a5dd94030a747095d0e129d86e
vitach <vvchagovets@gmail.com>
2022-02-01 20:07:43 +0300

    experiment with dvc

--- data/!readme.txt
+++ data/!readme.txt
@@ -1,4 +1,4 @@
-
+https://www.analyticsvidhya.com/blog/2022/01/overview-of-mlops-with-open-source-tools/?utm_source=feedburner&utm_medium=email
 
 Data
 https://sci2s.ugr.es/keel/dataset.php?cod=184#inicio
--- src/get_data.py
+++ src/get_data.py
@@ -35,3 +35,5 @@ if __name__=="__main__":
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
     data = get_data(config_path = parsed_args.config)
+    
+    print (data.describe())
